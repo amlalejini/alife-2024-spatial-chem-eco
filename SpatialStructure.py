da4cb8fd39dd5e6d668ba7ad3737f90280f9186c
John Shea <johnshea@Johns-MacBook-Air.local>
2023-07-05 13:32:29 -0400

    Made some additions to the graph functions.

--- SpatialStructure.py
+++ SpatialStructure.py
@@ -11,6 +11,7 @@ Aric A. Hagberg, Daniel A. Schult and Pieter J. Swart,
  in Proceedings of the 7th Python in Science Conference (SciPy2008),
  Gäel Varoquaux, Travis Vaught, and Jarrod Millman (Eds), (Pasadena, CA USA), pp. 11–15, Aug 2008
 """
+
 import argparse
 import networkx as nx
 
@@ -22,28 +23,55 @@ def gen_graph_well_mixed(nodes:int):
     return graph
 
 def gen_graph_toroidal_lattice():
-    pass
+    # return nx.toroidal_lattice_graph(nodes) # <-- Using the networkx generator
+    graph = nx.Graph()
+    row_name, rows = m
+    col_name, cols = n
+    graph.add_nodes_from((i, j) for i in rows for j in cols)
+    graph.add_edges_from(((i, j), (pi, j)) for pi, i in pairwise(rows) for j in cols)
+    graph.add_edges_from(((i, j), (i, pj)) for i in rows for pj, j in pairwise(cols))
+    return graph
 
 def gen_graph_comet_kite():
-    pass
+    # return nx.comet_kite_graph(nodes) # <-- Using the networkx generator
+    graph = nx.Graph()
+    graph.add_nodes_from()
+    graph.add_edges_from()
+    return graph
 
 def gen_graph_circular_chain():
-    pass
+    # return nx.circular_chain_graph(nodes) # <-- Using the networkx generator
+    graph = nx.Graph()
+    graph.add_nodes_from()
+    graph.add_edges_from()
+    return graph
 
 def gen_graph_linear_chain():
-    pass
+    # return nx.linear_chain_graph(nodes) # <-- Using the networkx generator
+    graph = nx.Graph()
+    graph.add_nodes_from()
+    graph.add_edges_from()
+    return graph
 
-def gen_graph_random_blah():
-    pass
+def gen_graph_random_blah(nodes:int, prob_edge:float, ):
+    # return nx.random_graph(nodes) # <-- Using the networkx generator
+    graph = nx.Graph()
+    graph.add_nodes_from()
+    graph.add_edges_from()
+    return graph
 
 def gen_graph_random_blah2():
-    pass
+    # return nx.complete_graph(nodes) # <-- Using the networkx generator
+    graph = nx.Graph()
+    graph.add_nodes_from()
+    graph.add_edges_from()
+    return graph
 
 def main():
     parser = argparse.ArgumentParser(
         usage="Program for generating graphs"
     )
-    parser.add_argument("--type", type = str, default = "well-mixed", choices = ["well-mixed"], help = "Type of graph to generate")
+    parser.add_argument("--type", type = str, default = "well-mixed", choices = ["well-mixed", "toroidal lattice", "comet-kite", "circular chain", "linear chain", "random"], help = "Type of graph to generate")
     parser.add_argument("--nodes", type = int, default = 10, help = "Number of nodes in graph")
 
     args = parser.parse_args()
@@ -54,11 +82,27 @@ def main():
     if graph_type == "well-mixed":
         graph = gen_graph_well_mixed(nodes = graph_nodes)
         print(graph)
+    if graph_type == "toroidal lattice":
+        graph = gen_graph_toroidal_lattice(nodes = graph_nodes)
+        print(graph)
+    if graph_type == "comet-kite:
+        graph = gen_graph_comet_kite(nodes = graph_nodes)
+        print(graph)
+    if graph_type == "circular chain":
+        graph = gen_graph_circular_chain(nodes = graph_nodes)
+        print(graph)
+    if graph_type == "linear chain":
+        graph = gen_graph_linear_chain(nodes = graph_nodes)
+        print(graph)
+    if graph_type == "random":
+        graph = gen_graph_random_blah(nodes = graph_nodes)
+        print(graph)
+    if graph_type == "random":
+        graph = gen_graph_random_blah2(nodes = graph_nodes)
+        print(graph)
     else:
         print("Unrecognized graph type!")
         exit(-1)
-
-
 
 
 if __name__ == '__main__':
