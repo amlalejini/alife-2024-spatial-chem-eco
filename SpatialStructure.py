cd7f517ceb3537317c0d06047d06504d01ec3368
Alexander Lalejini <amlalejini@gmail.com>
2023-07-12 14:44:09 -0400

    Implement comet-kite graph generator

--- SpatialStructure.py
+++ SpatialStructure.py
@@ -12,6 +12,7 @@ Aric A. Hagberg, Daniel A. Schult and Pieter J. Swart,
  Gäel Varoquaux, Travis Vaught, and Jarrod Millman (Eds), (Pasadena, CA USA), pp. 11–15, Aug 2008
 """
 import argparse
+import random
 import networkx as nx
 
 def gen_graph_well_mixed(nodes:int):
@@ -47,16 +48,38 @@ def gen_graph_toroidal_lattice(graph_width:int, graph_height:int):
             graph.add_edge(id, left)
     return graph
 
-# Pseudo code at the momentd
-def gen_graph_comet_kite(nodes:int, tails:int):
+# Algorithm from "Exploring and mapping the universe of evolutionary graphs
+# identifies structural properties affecting fixation and probability time"
+# (Moller et al)
+# TODO
+# - Add random number seed, set random number seed
+# - Write out the behavior of this function as a comment
+def gen_graph_comet_kite(core_size:int, num_tails:int, additional_tail_nodes:int = 0, seed:int = 1):
     # return nx.comet_kite_graph(nodes) # <-- Using the networkx generator
-    graph = nx.complete_graph(nodes)
-    for i in range(nodes):
-        graph.add_nodes_from()
-        graph.add_edges_from()
-        for i in range(tails):
-            graph.add_nodes_from()
-            graph.add_edges_from()
+    random.seed(seed)
+    # 1) Generate complete graph
+    graph = nx.complete_graph(core_size)
+    # If no nodes, return empty graph
+    if core_size < 1:
+        return graph
+    # 2) Pick existing node to attach tail
+    tail_root = 0
+    # 3) Attach t ("tails") nodes to the tail_root node
+    tail_nodes = [t for t in range(core_size, core_size + num_tails)]
+    graph.add_nodes_from(tail_nodes)
+    graph.add_edges_from([(tail_root, t) for t in tail_nodes])
+    if len(tail_nodes) < 1:
+        return graph
+    # 4) Attach any additional tail nodes to existing tails
+    for i in range(additional_tail_nodes):
+        attach_point = random.choice(tail_nodes)
+        new_node = core_size + num_tails + i
+        graph.add_node(new_node)
+        graph.add_edge(attach_point, new_node)
+        tail_nodes.append(new_node)
+    # print("Core nodes:", graph.nodes)
+    # print("Initial tail nodes:", tail_nodes)
+    # print("Edges: ", graph.edges)
     return graph
 
 def gen_graph_circular_chain(nodes:int):
@@ -97,6 +120,7 @@ def main():
     )
     parser.add_argument("--nodes", type = int, default = 10, help = "Number of nodes in graph")
     parser.add_argument("--tails", type = int, default = 2, help = "Number of tails conneted to graph")
+    parser.add_argument("--additional_tail_nodes", type = int, default = 2, help = "Number of nodes in tail part of graph")
     parser.add_argument("--height", type = int, default = 3, help = "Height of graph (for graph types where relevant)")
     parser.add_argument("--width", type = int, default = 3, help = "Width of the graph (for graph types where relevant)")
     parser.add_argument("--seed", type = int, default = 1, help = "Seed info")
@@ -107,6 +131,7 @@ def main():
     graph_type = args.type
     graph_nodes = args.nodes
     graph_tails = args.tails
+    graph_additional_tail_nodes = args.additional_tail_nodes
     graph_width = args.width
     graph_height = args.height
 
@@ -118,7 +143,12 @@ def main():
         graph = gen_graph_toroidal_lattice(graph_width=graph_width, graph_height=graph_height)
         print(graph)
     elif graph_type == "comet-kite":
-        graph = gen_graph_comet_kite(nodes = graph_nodes)
+        graph = gen_graph_comet_kite(
+            core_size = graph_nodes,
+            num_tails = graph_tails,
+            additional_tail_nodes = graph_additional_tail_nodes,
+            seed = args.seed
+        )
         print(graph)
     elif graph_type == "circular-chain":
         graph = gen_graph_circular_chain(nodes = graph_nodes)
@@ -127,7 +157,7 @@ def main():
         graph = gen_graph_linear_chain(nodes = graph_nodes)
         print(graph)
     elif graph_type == "random-barabasi-albert":
-        graph = gen_graph_random_barabasi_albert(nodes = graph_nodes, edges = args.edges, seed = args.seed )
+        graph = gen_graph_random_barabasi_albert(nodes = graph_nodes, edges = args.edges, seed = args.seed)
         print(graph)
     elif graph_type == "random-erdos-renyi":
         graph = gen_graph_random_erdos_renyi(nodes = graph_nodes, edge_prob = args.edge_prob, seed = args.seed)
